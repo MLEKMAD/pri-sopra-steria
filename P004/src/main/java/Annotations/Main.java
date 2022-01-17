7b34ee4e8184ceffec6d9ae115e93161f92f945e
arbaretaz.aurelien <aurelien.arbaretaz@orange.fr>
2022-01-17 10:02:46 +0100

    Chemin rapport

--- P004/src/main/java/Annotations/Main.java
+++ P004/src/main/java/Annotations/Main.java
@@ -8,8 +8,8 @@ import process.ProcessProgram;
 
 public class Main {
 	public static void main(String args[]) throws FileNotFoundException, IOException, CsvValidationException {
-		String fileCSV = "C:\\Users\\HamidLekmad\\git\\pri\\P004\\Rapport.csv";
-		String outputFile = "C:\\Users\\HamidLekmad\\git\\pri\\P004\\Rapport2.csv";
+		String fileCSV = "Rapport.csv";
+		String outputFile = "Rapport2.csv";
 		ProcessProgram testCSV = new ProcessProgram();
 		testCSV.processFile(fileCSV, outputFile);
 
